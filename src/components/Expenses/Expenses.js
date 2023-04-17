6c37354bfc6db4a35bba745a54351b3b30523f04
Mohamed <mohamedabuassaf@gmail.com>
2023-04-17 03:00:32 +0300

    init

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,43 +1,39 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
 import ExpenseFilter from "../ExpenseFilter/ExpenseFilter";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 
 function Expenses(props) {
-    
+
     const [clickedYear, setClickedYear] = useState('2020');
 
-    const filterChangeHnadler = (selectedYear) =>{
+    const filterChangeHnadler = (selectedYear) => {
         setClickedYear(selectedYear);
-    }
+
+    };
+
+    const filteredExpensesByDate = props.items.filter(expense => {
+        return (expense.date.getFullYear().toString() === clickedYear)
+    });
 
     return (
         <div>
-        <Card className="expenses">
-            <ExpenseFilter selected={clickedYear} onClickedYear={filterChangeHnadler}/>
-            <ExpenseItem
-                title={props.items[0].title}
-                amount={props.items[0].amount}
-                date={props.items[0].date}>
-            </ExpenseItem>
-            <ExpenseItem
-                title={props.items[1].title}
-                amount={props.items[1].amount}
-                date={props.items[1].date}>
-            </ExpenseItem>
-            <ExpenseItem
-                title={props.items[2].title}
-                amount={props.items[2].amount}
-                date={props.items[2].date}>
-            </ExpenseItem>
-            <ExpenseItem
-                title={props.items[3].title}
-                amount={props.items[3].amount}
-                date={props.items[3].date}>
-            </ExpenseItem>
-        </Card>
-    </div>
+            <Card className="expenses">
+                <ExpenseFilter selected={clickedYear} onClickedYear={filterChangeHnadler} />
+
+                {filteredExpensesByDate.map((expense) => (
+
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                ))
+                }
+            </Card>
+        </div>
     )
 
 }
